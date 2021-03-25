d54a0b72d07e9212ad015f2ed7342685140fa6f0
maneerat.m <51904844+zionarty@users.noreply.github.com>
2021-03-25 23:29:25 +0700

    basic_js_variable_1
    basic_js_variable_1

---
+++ JavascriptBasicPart_I/Variable_1.js
@@ -0,0 +1,23 @@
+/*
+3.ตัวแปรและประเภทข้อมูล
+    3.1 ตัวแปร
+        -ตัวแปรคือสิ่งที่เอาไว้เก็บข้อมูล
+        -และสามารถเปลี่ยนแปลงข้อมูลข้างในได้
+
+        console.log('Codecamp #5');
+
+        var message = 'Codecamp #5';
+        console.log(message);
+*/
+
+//กำหนดค่า
+    let message = 'Codecamp #5';
+    let age = 30;
+    alert(message);
+    console.log(message);
+
+//เปลี่ยนแปลงค่า
+    message='Hello';
+    console.log(message);
+    console.log(age);
+    console.log(age+10);
